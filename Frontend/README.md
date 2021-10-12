4b4be64bc5c8e5ebf95eae5e2d850fa5eb6fc2bf
Albert Gromek <alberto_gromek@hotmail.com>
2021-10-12 20:33:53 +1100

    Basic UI, with error handling and message display when rate limit is reached
    Based on create-react-app project!

---
+++ Frontend/README.md
@@ -0,0 +1,17 @@
+## Job Vacancies Front End
+
+First make sure the Backend project is running before starting this project. 
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### API
+
+The data for the Job Vacancies frontend will be read from the .NET Api project which should be setup
+to run on localhost port 44325.
+
+You can test the API by making a GET request to: https://localhost:44325/api/JobVacancies
+
---
+++ Frontend/src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+  const [display, setDisplay] = useState([]);
+  const [city, setCity] = useState([]);
+  const [countryCode, setCountryCode] = useState([]);
+
+  function fetchRequest() {
+    fetch(
+      `https://localhost:44396/weatherforecast/forecast?city=${city}&countryCode=${countryCode}`,
+      {
+        headers: {
+          ClientId: "Bob",
+          "content-type": "application/json",
+        },
+      }
+    )
+      .then(function (response) {
+        if (response.status === 200) {
+          return response.json();
+        }
+        if (response.status === 429) {
+          return "Rate Limit Reached";
+        }
+      })
+      .then((response) => {
+        setDisplay(response);
+      })
+      .catch((error) => setDisplay("An error has occured."));
+  }
+
+  return (
+    <div className="App">
+      <h1>Weather Forecast</h1>
+      <label>
+        Country Code (ISO-3661-2 Format):
+        <input
+          type="text"
+          value={countryCode}
+          onChange={(e) => setCountryCode(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        City:
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+      </label>
+      <br />
+      <button className="button" onClick={fetchRequest}>Submit</button>
+      <div>
+        <h2>{display}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
